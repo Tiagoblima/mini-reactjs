dbbb0ba32f72db1cb090d9a99fe8d3db6543c48d
tiago.blima <tiago.blima02@gmail.com>
2021-03-21 11:54:07 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useEffect, useState} from "react";
+import api from "./services/api";
 function App() {
+
+  let msg = "Hello World"
+  const [state, setState] = useState(null)
+  async function getData (){
+    const url = 'processInfo/Result/all/';
+    const response = await fetch(url);
+    const data =  (await api.get(url)).data
+    console.log(data)
+    return  data[0]["NomeUJPrincipalProcesso"]
+
+  }
+
+  getData().then(r => setState(r))
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { state }
         </p>
         <a
           className="App-link"
